5e6a66697a1220086711741d2a493aaec6cce0bf
shanjanapriya <priyashanjana2305@gmail.com>
2024-11-20 22:09:29 +0600

    update

--- js/scripts.js
+++ js/scripts.js
@@ -133,19 +133,173 @@
 
 
 
-var clickButton = document.getElementById("color_button");
-var showButton = document.getElementById("hide")
+// var clickButton = document.getElementById("color_button");
+// var showButton = document.getElementById("hide")
 
-clickButton.onclick = function () {
-    document.getElementById('box2').classList.add("show")
+// clickButton.onclick = function () {
+//     document.getElementById('box2').classList.add("show")
+// }
+
+// showButton.onclick = function () {
+//     document.getElementById("box2").classList.remove("show")
+// }
+
+
+// var divisionResult= divisionnu
+
+
+
+function number(c) {
+    var numbers=(9*c/5)+32;
+    return numbers;
 }
 
-showButton.onclick = function () {
-    document.getElementById("box2").classList.remove("show")
+var result=number(38);
+console.log(result);
+document.getElementById("fahr_1").innerHTML=result;
+
+
+
+function number(c) {
+    var numbers=(9*c/5)+32;
+    return numbers;
+}
+
+var result=number(24);
+console.log(result);
+document.getElementById("fahr_2").innerHTML=result;
+
+
+
+
+function number(c) {
+    var numbers=(9*c/5)+32;
+    return numbers;
+}
+
+var result=number(-1);
+console.log(result);
+document.getElementById("fahr_3").innerHTML=result;
+
+
+
+var classification = [ "John Hill", "Mary Jane" , "Gary Vee" , "paricia Mills" , "Helen Hall" , "Paul Green"];
+classification.shift();
+console.log(classification);
+
+
+var classification = [  "Mary Jane" , "Gary Vee" , "paricia Mills" , "Helen Hall" , "Paul Green"];
+classification.shift();
+console.log(classification);
+
+
+var classification = [  "Gary Vee" , "paricia Mills" , "Helen Hall" , "Paul Green"];
+classification.shift();
+console.log(classification);
+document.getElementById("best_students").innerHTML=classification
+
+
+
+
+var course = {
+    'title' : "Learn to Code in Python 3" ,
+    'categories' : ['programming', 'tecnology', 'python'],
+    '5_starts_reviews': 420,
+    '4_starts_reviews':80,
+    '3_starts_reviews':33,
+    '2_starts_reviews':20,
+    '1_starts_reviews':4
 }
 
 
+console.log(course.title);
+document.getElementById("course_title").innerHTML=course.title
 
+console.log(course['categories'][0]);
+document.getElementById("main_category").innerHTML=course['categories'][0]
+var shoppingList = ["Milk", "Butter", "Juice", "Bread", "Beer", "Rice", "Potatoes", "Chocolate"];
+shoppingList.pop();
+shoppingList.shift();
+shoppingList.unshift("Chocolate");
+console.log(shoppingList);
+
+
+
+var shoppingList = ["Milk", "Butter", "Juice", "Bread", "Beer", "Rice", "Potatoes", "Chocolate"];
+shoppingList.push("Cheese");
+shoppingList.push("Eggs");
+
+
+console.log(shoppingList);
+
+
+
+function sum_args(num1,num2,num3,num4,num5) {
+    var sum  = num1 + num2 + num3 + num4 + num5;
+    console.log(sum);
+}
+sum_args(420,80,33,20,4);
+var average=(420/557)*100;
+console.log(average);
+var roundNumber = Math.round ( 75.40394973070018);
+console.log(roundNumber);
+var name ="75";
+name += "%";
+console.log(name)
+document.getElementById("reviews_5_stars").innerHTML=name;
+document.getElementById("click-me").onclick = function(){
+    alert('are u human');
+};
+
+
+document.getElementById("hover-me").onclick = function(){
+    alert('take your cap');
+};
+
+document.getElementById("leave-me").onclick = function(){
+    alert('you moved too much');
+};
+
+document.getElementById("see-me").onclick = function() {
+    alert('do not go there');
+};
+
+
+var button = document.getElementById("color_button");
+button.onclick = function() {
+    document.getElementById("button_one") .style .display ="block";
+}
+
+
+document.getElementById("color_button").onclick = function() {
+    this.style['background-color'] ="orange";
+};
+
+
+// document.getElementById("button_one").onclick = function() {
+//     this.style["background-color"] ="aqua";
+//     this.style.transform = "translateY(-10)";
+// };
+
+
+// var button = document.getElementById("button_two");
+
+// button.onclick=function() {
+//     button.style["background-color"]="red";
+//     button.style.transform="translateX(10px)";
+// };
+
+
+// var clickButton = document.getElementById("color_button");
+// var showButton = document.getElementById("button_two");
+
+// clickButton.onclick=function() {
+//     document.getElementById("button_one").classList.toggle("show")
+// }
+
+// showButton.onclick=function() {
+//     document.getElementById("button_one").classList.toggle("show")
+// }
 
 
 
